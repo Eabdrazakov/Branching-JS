bd544607aa6545a1b60e79d78c1d632d1c1eac87
Padma Patil <padma@email.com>
2023-11-01 13:47:35 -0700

    Add link

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+window.onload = function () {
+
+    const form = document.querySelector("form");
+    form.onsubmit = function (event) {
+        event.preventDefault();
+
+        let vote = document.getElementById("vote");
+        vote.setAttribute("class", "hidden");
+        let under21 = document.getElementById("under-21");
+        under21.setAttribute("class", "hidden");
+
+        const age = parseInt(document.querySelector("input#age").value);
+
+        if (age > 21) {
+            vote.removeAttribute("class");
+        } else if (age === 21) {
+            window.alert("Have some fun, you are just 21!");
+            vote.removeAttribute("class");
+        } else {
+            under21.removeAttribute("class");
+        }
+    };
+};
